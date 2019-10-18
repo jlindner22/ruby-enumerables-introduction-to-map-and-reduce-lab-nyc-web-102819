69dd6adcb205ca4c16bf8c1ba154d04f2f9b9ae7
Jen Lindner <jenlindner22@gmail.com>
2019-10-18 17:44:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,7 +39,7 @@ end
 def reduce_to_total(source_array, starting_point)
 count = 0
 while count < source_array.length do
-source_array = source_array[count] + starting_point
+source_array = source_array.sum(starting_point)
 print source_array[count]
   count += 1
 end 
