6c3e0d61288e5ca3b3c06c26277159f859a3b48b
Jen Lindner <jenlindner22@gmail.com>
2019-10-18 16:48:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,72 @@
-# My Code here....
+def map_to_negativize(source_array)
+count = 0
+negative = -1
+while count < source_array.length do
+source_array[count] = source_array[count] * negative
+source_array[count]
+count += 1
+end
+source_array
+end
+
+ def map_to_no_change(source_array)
+   array = source_array
+ end
+ 
+def map_to_double(source_array)
+count = 0
+double = 2
+while count < source_array.length do
+source_array[count] = source_array[count] * double
+source_array[count]
+count += 1
+end
+source_array
+end
+
+def map_to_square(source_array)
+count = 0
+square = 2
+while count < source_array.length do
+ source_array[count] = source_array[count]**square
+ source_array[count]
+  count += 1
+end 
+source_array
+end
+
+#need to grab value from array and add to the current total
+# def reduce_to_total(source_array, starting_point)
+# count = 0
+# source_array = source_array(starting_point)
+# while count < source_array.length do
+# source_array[count] = source_array[count] + source_array
+# print source_array[count]
+#   count += 1
+# end 
+# source_array
+# end
+
+# def reduce_to_total(source_array, starting_point)
+#   source_array = 0
+#   source_array.each do |starting_point|
+#     source_array += starting_point
+#   end
+#   source_array 
+# end
+
+
+# reduce_to_all_true(source_array)
+
+def reduce_to_any_true(source_array)
+  i = 0 # set up a i for the enumeration of the passengers collection
+  while i < source_array.length do # a loop for each element
+    # Stop enumerating and return true if any passenger is
+    # coughing or sneezing
+    if source_array[i] == "true"
+     print true
+  end
+      i += 1
+  end
+   false
+end
